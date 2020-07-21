960197b7d5b5e9982fab405cd335d5bba1b794d9
mari <mari.17ymy@gmail.com>
2020-07-21 00:16:51 -0400

    imagen del disco

--- lib/src/pages/music_player_page.dart
+++ lib/src/pages/music_player_page.dart
@@ -9,10 +9,66 @@ class MusicPlayerPage extends StatelessWidget {
       body: SafeArea(
         child: Column(
           children: [
-            CustomAppBarWidget()
+            CustomAppBarWidget(),
+            _ImagenDiscoDuracion()
           ],
         ),
       )
+    );
+  }
+}
+
+class _ImagenDiscoDuracion extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        _ImagenDisco()
+      ],
+    );
+  }
+}
+
+class _ImagenDisco extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(20.0),
+      width: 250.0, height: 250.0,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(200.0),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Image(image: AssetImage('assets/aurora.jpg')),
+            Container(
+              width: 25.0, height: 25.0,
+              decoration: BoxDecoration(
+                color: Colors.black38,
+                borderRadius: BorderRadius.circular(25.0)
+              ),
+            ),
+            Container(
+              width: 18.0, height: 18.0,
+              decoration: BoxDecoration(
+                color: Color(0xff1C1C25),
+                borderRadius: BorderRadius.circular(25.0)
+              ),
+            )
+          ],
+        )
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(200.0),
+        gradient: LinearGradient(
+          colors: [
+            Color(0xff484750),
+            Color(0xff1E1C24)
+          ]
+        )
+      ),
     );
   }
 }
--- lib/src/widgets/custom_appbar.dart
+++ lib/src/widgets/custom_appbar.dart
@@ -4,18 +4,19 @@ class CustomAppBarWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        SizedBox(width: 10.0),
-        Icon(Icons.chevron_left, size: 40.0,),
-        Spacer(),
-        Icon(Icons.laptop),
-        SizedBox(width: 10.0),
-        Icon(Icons.headset),
-        SizedBox(width: 10.0),
-        Icon(Icons.mobile_screen_share),
-        SizedBox(width: 10.0),
-      ],
+    return Container(
+      padding: EdgeInsets.all(10.0),
+      child: Row(
+        children: [
+          Icon(Icons.chevron_left, size: 40.0,),
+          Spacer(),
+          Icon(Icons.laptop),
+          SizedBox(width: 10.0),
+          Icon(Icons.headset),
+          SizedBox(width: 10.0),
+          Icon(Icons.mobile_screen_share),
+        ],
+      ),
     );
   }
 }
