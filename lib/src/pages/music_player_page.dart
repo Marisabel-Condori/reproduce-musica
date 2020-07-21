f5afd406bc2b3a2609ba1bd38e4c76a98eaa9ad9
mari <mari.17ymy@gmail.com>
2020-07-20 23:33:41 -0400

    inicio de proyecto

---
+++ lib/src/pages/music_player_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MusicPlayerPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('hola'),
+    );
+  }
+}
